25f7aef03963b7d27334ad1d596808ee2cb3a501
B00bleaTea <71613062+B00bleaTea@users.noreply.github.com>
2021-04-25 23:39:35 +0000

    Create __main__.py

---
+++ __main__.py
@@ -0,0 +1,7 @@
+for num in range(-1_000_000_000, 1_000_000_000):
+    print(num)
+    with open("ifelseven.py", "a") as f:
+        f.write(f'\nif n == {num}:\nprint(\"{"even" if num % 2 == 0 else "odd"}\")')
+
+with open("ifelseven.py", "a") as f:
+    f.write(f'\n')
