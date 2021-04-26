fdc0ac3f3ab7a97c651f0ccbe1e3f9b4bf2439e4
B00bleaTea <71613062+B00bleaTea@users.noreply.github.com>
2021-04-26 00:08:18 +0000

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,7 +1,16 @@
-for num in range(-1_000_000_000, 1_000_000_000):
-    print(num)
-    with open("ifelseven.py", "a") as f:
-        f.write(f'\nif n == {num}:\nprint(\"{"even" if num % 2 == 0 else "odd"}\")')
+import functools
 
-with open("ifelseven.py", "a") as f:
-    f.write(f'\n')
+
+@functools.cache
+def main():
+    for num in range(-1_000_000_000, 1_000_000_000):
+        print(num)
+        with open("ifelseven.py", "a") as f:
+            f.write(f'\nif n == {num}:\nprint(\"{"even" if num % 2 == 0 else "odd"}\")')
+
+    with open("ifelseven.py", "a") as f:
+        f.write(f'\n')
+
+
+if __name__ == "__main__":
+    main()
